c0da19da97f826e0e636b1792c6d84beb19a7d56
huynq2k4 <huynq.contact@gmail.com>
2024-07-17 08:35:45 +0700

    Adjust the num_workers in DataLoader

--- utility.py
+++ utility.py
@@ -177,11 +177,11 @@ class Datasets():
                                                   self.num_bundles, self.num_items)
 
         self.train_loader = DataLoader(
-            self.bundle_train_data, batch_size=batch_size_train, shuffle=True, num_workers=10)
+            self.bundle_train_data, batch_size=batch_size_train, shuffle=True, num_workers=4)
         self.val_loader = DataLoader(
-            self.bundle_val_data, batch_size=batch_size_test, shuffle=False, num_workers=20)
+            self.bundle_val_data, batch_size=batch_size_test, shuffle=False, num_workers=4)
         self.test_loader = DataLoader(
-            self.bundle_test_data, batch_size=batch_size_test, shuffle=False, num_workers=20)
+            self.bundle_test_data, batch_size=batch_size_test, shuffle=False, num_workers=4)
 
     def combine_graph(self, pairs_list, shape, tag):
         pairs = np.concatenate(pairs_list, axis=0)
